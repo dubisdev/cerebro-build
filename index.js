2239a7237e64c233f054c6da94aa88538c333d30
dubisdev <dubisdev@gmail.com>
2022-09-05 23:14:23 +0200

    feat: add test command

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-switch (process.argv[2]) {
-  case "build":
-    import("./scripts/build.js");
-    break;
-  case "clear":
-    import("./scripts/clear.js");
-    break;
-  case "dev":
-    import("./scripts/dev.js");
-    break;
-  default:
-    console.error("Invalid-Command - Usage: npm run (build|clear|dev)");
-    process.exit(1);
+const scripts = {
+  build: () => import("./scripts/build.js"),
+  start: () => import("./scripts/dev.js"), // both start and dev are the same
+  dev: () => import("./scripts/dev.js"),
+  test: () => import("./scripts/test.js"),
+  clear: () => import("./scripts/clear.js"),
+  default: () => {
+    console.error("Invalid-Command - Usage: npm run (build|clear|dev|start|test)")
+    process.exit(1)
+  }
 }
+
+const script = scripts[process.argv[2]] || scripts.default 
+
+script()
